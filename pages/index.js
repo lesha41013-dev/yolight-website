14f90678b0432dd3ab6d8c44f52bd42bc7aec118
lesha41013-dev <lesha41013@gmail.com>
2026-03-11 11:30:57 +0800

    Create index.js
    import Head from 'next/head';
    export default function Home() {
      return (
        <>
          <Head>
            <title>Yolight - 面部瑜伽App</title>
            <meta name="description" content="Yolight面部瑜伽App免费下载" />
          </Head>
          <div style={{ padding: '2rem', textAlign: 'center' }}>
            <h1>Yolight - 面部瑜伽App</h1>
            <p>网站部署中...</p>
          </div>
        </>
      );
    }

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Yolight - 面部瑜伽App</title>
+        <meta name="description" content="Yolight面部瑜伽App免费下载" />
+      </Head>
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Yolight - 面部瑜伽App</h1>
+        <p>网站部署中...</p>
+      </div>
+    </>
+  );
+}
